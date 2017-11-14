a5d76bd769dcfe2683dd0525e93c2a5ee56112a3
KevinDNF <KevinNavarroFaubla@gmail.com>
2017-11-14 15:13:10 +0000

    Primary screen display fixed when more than 1 screen conn. screen.sh is a bodged scripts for a really bad secondary screen I was using. --> Will be altered to a fully functional screen detector script.

---
+++ .config/scripts/makePython.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+
+i3 exec $term -e ranger Documents/Y01/INTPROG/Python
+
+i3 split v
+i3 exec $term -e ranger Documents/Y01/INTPROG/Python
+
+i3 split h
+i3 exec $term -e  cd Documents/Y01/INTPROG/Python
+
+
+
+
+
